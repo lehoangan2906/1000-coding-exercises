04deb2ef673a57c168532aaefbf73c1a33e51cb9
Le Hoang An <75709930+lehoangan2906@users.noreply.github.com>
2021-05-31 00:45:40 +0700

    Add files via upload

---
+++ bài 119/bài 119/main.cpp
@@ -0,0 +1,32 @@
+// Bài 119: Viết hàm liệt kê tất cả các số nguyên tố nhỏ hơn n
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+void SieveOfEratosthenes (int n){
+
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+    
+    for (int p = 2; p*p <= n; p++){
+ 
+        if (prime[p] == true){
+            for (int i = p*p; i <= n; i += p)
+            prime [i] = false;
+        }
+    }
+    for ( int p = 2; p < n; p++)
+    if (prime[p])
+        cout << p << " ";
+}
+
+
+
+int main(){
+    int n;
+    cout << "nhap n: ";
+    cin >> n ;
+    SieveOfEratosthenes(n);
+    return 0;
+}
