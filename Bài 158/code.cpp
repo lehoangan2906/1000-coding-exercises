afe99200d970eb306e8847d3a004ff5d45600aeb
Le Hoang An <75709930+lehoangan2906@users.noreply.github.com>
2021-06-25 14:29:18 +0700

    Add files via upload

---
+++ Bài 158/code.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#define MAX 100
+using namespace std;
+
+void inputArray(float A[], int &n){
+    do{
+        printf("input the size of the array: ");
+        scanf("%d", &n);
+        if(n <= 0 || n > MAX){
+            printf("\ninvalid number");
+        }
+    }while(n <= 0 || n > MAX);
+
+    for(int i = 0; i < n; i++){
+        printf("\nInput the element A[%d]: ", i);
+        scanf("%f", &A[i]);
+    }
+}
+
+void outputArray(float A[], int n){
+    for (int i = 0; i < n; i++){
+        printf("\nA[%d] = %f", i, A[i]);
+    }
+}
+
+void solve(float A[],int n){
+    float max = A[0];
+    float min = A[0];
+    for(int i = 0; i < n; i++){
+        max = (A[i] > max) ? A[i] : max;       // if A[i] > max is true the, then max = A[i], if it is false then max = max
+        min = (A[i] < min) ? A[i] : min; 
+    }
+
+    float B = (abs(max) > abs(min)) ? abs(max) : abs(min);
+
+    printf("\nGia tri x sao cho doan [-x, x] chua tat ca cac gia tri trong mang la: %f;", B);
+}
+
+int main(){
+    int n;
+    float A[MAX];
+
+    inputArray(A, n);
+    outputArray(A, n);
+    solve(A, n);
+
+    return 0;
+}
